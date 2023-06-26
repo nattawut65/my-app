7c4b6c458160b29f8c64330136da8156d79385a8
nattawut65 <kinggm009@gmail.com>
2023-06-27 00:36:41 +0700

    homework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import { useState, useEffect } from 'react';
+
+function isPrimeNumber(num) {
+  if (num === 0 || num === 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 function App() {
+  const [num, setNum] = useState(0);
+  const [isPrime, setIsPrime] = useState(false);
+
+  useEffect(() => {
+    setIsPrime(isPrimeNumber(num));
+  }, [num]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="MyContent">
+        <h2>{num}</h2>
+        <p>{isPrime ? 'เป็นจำนวนเฉพาะ' : 'ไม่เป็นจำนวนเฉพาะ'}</p>
+        <button onClick={() => setNum(num + 1)}>เพิ่ม</button>
+        <button onClick={() => setNum(num - 1)}>ลด</button>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ Nattawut khempan</span>
+        </div>
+    );
+}
+
+export default Footer;
